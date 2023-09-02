2f86b5dd16c53192d9cbaa89e07b2918c2d3f648
Kyi <khantkhant528@gmail.com>
2023-09-03 00:33:00 +0700

    added input, button and result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState('');
+  const [result, setResult] = useState(null);
+
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const fetchGender = async () => {
+    try {
+      const response = await axios.get(`https://api.genderize.io/?name=${name}`)
+      setResult(response.data);
+
+    } catch (err) {
+      setResult(null);
+
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Guessing Gender By Name</h1>
+      <input type='text' placeholder='Enter a name' value={name} onChange={handleChange} />
+      <button onClick={fetchGender}>Click</button>
+      {result && (
+        <div>
+          <p>Name: {result.name}</p>
+          <p>Gender: {result.gender}</p>
+          <p>Probability: {result.probability}</p>
+          <p>Count: {result.count}</p>
+        </div>
+      )}
     </div>
   );
 }
